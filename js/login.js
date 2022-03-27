f3b5762fbca781ccc222c7198b9b207f7c032088
WaltrickBradock <90482470+WaltrickBradock@users.noreply.github.com>
2022-03-26 23:29:15 -0300

    Js

--- js/login.js
+++ js/login.js
@@ -4,9 +4,24 @@ function validate(){
     okpassword = document.getElementById("OKPassword")
     phone = document.getElementById("Phone")
 
-    var regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
 
-    if (email.match(regexEmail)){
-    
+    var regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+
+    var regexPhone = /^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/
+
+    if(okpassword != password){
+        return (false)
+    }else{
+        return (true)
     }
+
+    // deve conter ao menos um dígito
+    // deve conter ao menos uma letra minúscula
+    // deve conter ao menos uma letra maiúscula
+    // deve conter ao menos um caractere especial
+    //deve conter ao menos 8 dos caracteres mencionados
+    
+    console.log(regexPassword.test(password))
+
 }
