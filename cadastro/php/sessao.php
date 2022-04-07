3211d3d53d6a242739c55ad8aed2b4f02167a052
WaltrickBradock <90482470+WaltrickBradock@users.noreply.github.com>
2022-04-07 03:04:05 -0300

    4/7

--- cadastro/php/sessao.php
+++ cadastro/php/sessao.php
@@ -1,5 +1,6 @@
 <?php
 
-    
+    //https://www.devmedia.com.br/criando-sessao-para-login-no-php/27347
+    //Usa o site ai como base
 
 ?>
---
+++ login/php/sessao.php
@@ -0,0 +1,6 @@
+<?php 
+
+    //https://www.devmedia.com.br/criando-sessao-para-login-no-php/27347
+    
+
+?>
