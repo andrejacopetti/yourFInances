92e756ce96db0c5e6e8dc6eb68eeebff069c2114
WaltrickBradock <90482470+WaltrickBradock@users.noreply.github.com>
2022-04-07 02:40:40 -0300

    Email.php

---
+++ login/php/E-mail.php
@@ -0,0 +1,31 @@
+
+
+<?php
+    //so pra nao perder
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer-master/src/Exception.php';
+    require 'PHPMailer-master/src/PHPMailer.php';
+    require 'PHPMailer-master/src/SMTP.php';
+    $mail = new PHPMailer();
+    // Configuração
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
+    $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
+    // Detalhes do envio de E-mail
+    $mail->Username = '<usuario-email-remetente>';
+    $mail->Password = '<senha-email-remetente>';
+    $mail->SetFrom('<email-remetente>', '<remetente>');
+    $mail->addAddress('<email-destinatario>', '');
+    $mail->Subject = "<titulo>";
+    $mail->msgHTML("<mensagem>");
+    $mail->send();
+
+?>
