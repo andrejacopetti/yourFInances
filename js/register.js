60aa48b7522925d1569cd8da64f840cda903a864
WaltrickBradock <90482470+WaltrickBradock@users.noreply.github.com>
2022-04-19 17:20:20 -0300

    ooo

--- js/register.js
+++ js/register.js
@@ -7,7 +7,11 @@ function validate(){
     var regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
 
     var regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
-
+    // deve conter ao menos um dígito
+    // deve conter ao menos uma letra minúscula
+    // deve conter ao menos uma letra maiúscula
+    // deve conter ao menos um caractere especial
+    //deve conter ao menos 8 dos caracteres mencionados
     var regexPhone = /^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/
 
 
@@ -22,11 +26,7 @@ function validate(){
         return (true)
     }
 
-    // deve conter ao menos um dígito
-    // deve conter ao menos uma letra minúscula
-    // deve conter ao menos uma letra maiúscula
-    // deve conter ao menos um caractere especial
-    //deve conter ao menos 8 dos caracteres mencionados
+    
     
     
 
--- php/Email-Confirm.php
+++ php/Email-Confirm.php
@@ -1,10 +1,12 @@
 <?php
-    
+
+    $emaildest = "<script>document.write(email)</script>";
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
-    require 'PHPMailer-master/src/._Exception.php';
-    require 'PHPMailer-master/src/._PHPMailer.php';
-    require 'PHPMailer-master/src/._SMTP.php';
+    require 'PHPMailer-master/src/Exception.php';
+    require 'PHPMailer-master/src/PHPMailer.php';
+    require 'PHPMailer-master/src/SMTP.php';
 
     $mail = new PHPMailer();
     // Configuração
@@ -17,12 +19,18 @@
     $mail->Host = 'smtp.gmail.com';
     $mail->Port = 465;
     // Detalhes do envio de E-mail
-    $mail->Username = '<usuario-email-remetente>';
-    $mail->Password = '<senha-email-remetente>';
-    $mail->SetFrom('<email-remetente>', '<remetente>');
-    $mail->addAddress('<email-destinatario>', '');
-    $mail->Subject = "<titulo>";
-    $mail->msgHTML("<mensagem>");
+    $mail->Username = 'yourfinance2103@gmail.com';
+    $mail->Password = 'finance2103';
+    $mail->SetFrom('yourfinance2103@gmail.com', 'YourFinance');
+    $mail->addAddress($emaildest, '');
+    $mail->Subject = "Confirmar Criacao de conta";
+    $mail->msgHTML("Clique no link para confirmar seu email");
     $mail->send();
+    
+    /// anotando pra nao esquecer 
+    /// logica 
+    /// fazer funcao com tudo q esta em cima.
+    /// mandar essa funcao pro js register
+    /// executar funcao la, apos verificar o regex, entao se regex email,senha etc == true, roda a funcao pra mandar email d comfirmacao
 
 ?>
