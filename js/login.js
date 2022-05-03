de0ea30060b1a59fdab6da4e8fb8cb56a844fe62
Vinicius Waltrick <90482470+WaltrickBradock@users.noreply.github.com>
2022-05-03 19:17:47 -0300

    qu

--- js/login.js
+++ js/login.js
@@ -1,8 +1,17 @@
-var email = document.getElementById('Email')
+function ValidateEmail(){
+    email = document.getElementById("Email")
+    password= document.getElementById("Password")
+    okpassword = document.getElementById("OKPassword")
+    phone = document.getElementById("Phone")
 
-function ValidateEmail() 
-{
-                
-    
+    var regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+    var regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+
+    if(console.log(regexEmail.test(email.value)) == true && console.log(regexPassword.test(password.value)) == true){
+        
+    }else{
+        
+    }
     
 }
