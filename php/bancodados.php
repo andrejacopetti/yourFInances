c14b91cb682af225212808621430ba23aa986db7
andrejacopetti <87780236+andrejacopetti@users.noreply.github.com>
2022-05-03 21:10:10 -0300

    banco de dados php
    criei o banco de dados

---
+++ php/bancodados.php
@@ -0,0 +1,25 @@
+<?php
+  
+  
+  
+  $emailbd = $_POST["Email"];
+  $senhabd = $_POST["Password"];
+  $phonebd = $_POST["Phone"];
+
+
+  $conexao = mysqli_connect("localhost:3307", "root", "root", "bd_seguranca");
+
+  //mysqli_query($conexao, "INSERT INTO usuario(nome, usuario, senha) VALUES('$nome', '$usuario', '$senha')");
+
+
+  /*$resultado = mysqli_query($conexao, "SELECT * FROM usuario WHERE usuario = '$usuario' AND senha = '$senha' ");
+
+  $qtde = mysqli_num_rows($resultado);
+
+  echo $qtde; 
+  */
+
+  
+
+
+?>
